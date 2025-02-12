19f967981a49dd80d12b7ed26f5e5e2ccb5ebd16
SrbastianM <sebas907.l@gmail.com>
2025-02-12 13:13:22 -0500

    feat: Now users can activated their users using a token sending to their emails 	- fix: Problem with unmarshal(pointer) in the activeUserHandler 	- add the logic from the GetForToken 	- Add new end point to activate new users "v1/users/activated"

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -27,6 +27,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/foods/:id", app.updateFoodHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/foods/:id", app.deleteFoodHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
 	return app.recoverPanic(app.rateLimit(router))
 }
